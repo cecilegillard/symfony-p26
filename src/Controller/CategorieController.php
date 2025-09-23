8d4a4431b421924d5fffded36b13c37c239c39e2
Cecile Legrand <legrand.cecile.59@gmail.com>
2025-09-23 11:08:22 +0200

    Formulaire d'jaout de catégorie

--- src/Controller/CategorieController.php
+++ src/Controller/CategorieController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
+use App\Form\CategorieType;
 use App\Repository\CategorieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,6 +22,23 @@ final class CategorieController extends AbstractController
         ]);
     }
 
+    #[Route('/categorie/add', name: 'app_categorie_add')]
+    public function add(Request $request, EntityManagerInterface $entityManager) {
+        $categorie = new Categorie();
+        $form = $this->createForm(CategorieType::class, $categorie);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            // on demande à Doctrine de s'occuper de l'object $categorie
+            $entityManager->persist($categorie);
+            // On execute les instructions SQL
+            $entityManager->flush();
+            return $this->redirectToRoute('app_categorie');
+        }
+
+        return $this->render('categorie/add.html.twig',
+            ["form" => $form]);
+    }
+
     #[Route('/categorie/{id}', name: 'app_categorie_show')]
     public function show(CategorieRepository $categorieRepository, $id): Response
     {
@@ -26,4 +47,6 @@ final class CategorieController extends AbstractController
             'categorie' => $categorie,
         ]);
     }
+
+
 }
